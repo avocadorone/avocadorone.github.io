a1f8e4f956730829e71e4b71135d5918f6fa7781
avocadorone <81843914+avocadorone@users.noreply.github.com>
2021-04-03 14:31:19 +0900

    Update date.php

--- date.php
+++ date.php
@@ -5,11 +5,9 @@
 <title>POST_SAMPLE</title>
 </head>
 <body>
-<form method="POST" action="sample-post2.php">
-	<input type="checkbox" name="fruits[]" value="apple"><label for= "apple">apple</label>
-	<input type="checkbox" name="fruits[]" value="orange"><label for= "orange">orange</label>
-	<input type="checkbox" name="fruits[]" value="melon"><label for= "melon">melon</label><br />
-	<input type="submit" value="送信" />
+<?php
+	echo "デートと言えば「".$_POST["data"] ."」です。";
+?>
 </form>
 </body>
 </html>
